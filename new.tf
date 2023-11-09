c138ca40d6fa5d93d125771eaebf18469c720db0
kovvuridharani <69035971+kovvuridharani@users.noreply.github.com>
2023-11-09 16:00:34 +0530

    Update new.tf

--- new.tf
+++ new.tf
@@ -5,7 +5,6 @@ region="ap-south-1"
 resource "aws_instance" "test-server" {
   ami           = "ami-02e94b011299ef128"
   instance_type = "t2.medium"
-  key_name = "my-key"
   tags = {
     Name = "test-server"
     Stage = "testing"
