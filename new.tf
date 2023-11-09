876014663d443fbd960626312790bf4a2c3bf596
kovvuridharani <69035971+kovvuridharani@users.noreply.github.com>
2023-11-09 15:30:44 +0530

    Create new.tf

---
+++ new.tf
@@ -0,0 +1,13 @@
+provider "aws"{
+}
+resource "aws_instance" "test-server" {
+  ami           = "ami-02e94b011299ef128"
+  availability_zone = "ap_south_1a"
+  instance_type = "t2.micro"
+  key_name = "my-key"
+  tags = {
+    Name = "test-server"
+    Stage = "testing"
+    Location = "chennai"
+  }
+}
