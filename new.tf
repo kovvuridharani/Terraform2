6e9c801cecaad1ccbe9c2a37fca821898a2337bc
kovvuridharani <69035971+kovvuridharani@users.noreply.github.com>
2023-11-09 15:50:50 +0530

    Update new.tf

--- new.tf
+++ new.tf
@@ -3,7 +3,7 @@ provider "aws"{
 resource "aws_instance" "test-server" {
   ami           = "ami-02e94b011299ef128"
   availability_zone = "ap_south_1a"
-  instance_type = "t2.micro"
+  instance_type = "t2.medium"
   key_name = "my-key"
   tags = {
     Name = "test-server"
